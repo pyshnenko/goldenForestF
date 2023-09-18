7b2e67278ad36ad2f0239a55aea9983ae28380a5
pyshnenko <pyshnenko94@yandex.ru>
2023-09-18 16:21:01 +0300

    делаем казну

--- src/pages/treasurer/Treasury.tsx
+++ src/pages/treasurer/Treasury.tsx
@@ -23,6 +23,10 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import TextField from '@mui/material/TextField';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateRangeCalendar } from '@mui/x-date-pickers-pro/DateRangeCalendar';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Skeleton from '@mui/material/Skeleton';
+import { IconButton } from '@mui/material';
+import InfoIcon from '@mui/icons-material/Info';
 
 dayjs.locale('ru')
   
@@ -69,13 +73,14 @@ const headCells: readonly HeadCell[] = [
 
 export default function Treasury (args: any) {
     const [ total, setTotal ] = useState<number>(0);
-    const [ ready, setReady ] = useState<boolean>(false);
+    const [ ready, setReady ] = useState<{gold: boolean, total: boolean}>({gold: false, total: false});
     const [ filtr, setFiltr ] = useState<{str: string, date: {up: number, down: number}}>({str: '', date: {up: Number(new Date()) + 100000, down: 0}})
     const { user, userFull } = useAuth();
     const trig = useRef<boolean>(true);
     const [page, setPage] = useState(0);
     const [ rows, setRows ] = useState<rowsData[]>([]);
-    const [rowsPerPage, setRowsPerPage] = useState(5);
+    const [rowsPerPage, setRowsPerPage] = useState(50);
+    const [ width, setWidth ] = useState<number>(window.innerWidth);
   
     const handleChangePage = (event: unknown, newPage: number) => {
       setPage(newPage);
@@ -95,13 +100,12 @@ export default function Treasury (args: any) {
         () => {
             buf=[];
             rows.map((item) => {
-                if (item.login.includes(filtr.str)&&(item.date>= filtr.date.down)&&(item.date<=filtr.date.up)) buf.push(item);
+                if (item.login.toLocaleLowerCase().includes(filtr.str.toLocaleLowerCase())&&(item.date>= filtr.date.down)&&(item.date<=filtr.date.up)) buf.push(item);
             });
-            buf.slice(
+            return buf.slice(
                 page * rowsPerPage,
                 page * rowsPerPage + rowsPerPage,
             )
-            return buf
         },
         [page, rowsPerPage, ready, filtr],
     );
@@ -115,129 +119,138 @@ export default function Treasury (args: any) {
                 setRows(result.data.res.data.history);
                 setTotal(result.data.res.data.total);
             })
+            
+            const handleResize = (event: any) => {
+                setWidth(event.target.innerWidth);
+            };
+
+            window.addEventListener('resize', handleResize);
+            return () => {
+                window.removeEventListener('resize', handleResize);
+            };
         }
     },[])
 
     useEffect(()=>{
         setRows(rows);
-        if (rows.length) setReady(true);
-        else setReady(false);
+        if (rows.length) setReady({...ready, gold: true});
+        else setReady({...ready, gold: false});
     }, [total])
 
     return (
-        <Fade in={ready}>
-            <Box sx={{    
-                display: 'flex',
-                flexDirection: 'column',
-                alignContent: 'center',
-                alignItems: 'center'}}
-            >
-                <Accordion>
-                    <AccordionSummary
-                        expandIcon={<ExpandMoreIcon />}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
-                    >
-                        <Typography>Деньги в кошельках</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails
-                        sx={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center'
-                        }}
-                    >
-                        <Box>
-                            <Typography>Денежек у нас {total}</Typography>
-                        </Box>
-                        <Box>
-                            <TextField label="Фильтр по имени" variant="outlined" onChange={({target}: any)=>{
-                                setFiltr({...filtr, str: target.value})
-                            }} />                            
-                            <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ru">
-                                <DateRangeCalendar
-                                    defaultValue={[dayjs('2022-04-17'), dayjs('2022-04-21')]}
-                                />
-                            </LocalizationProvider>
-                        </Box>
-                        <Box>
-                            <Box sx={{ width: '100%' }}>
-                                <Paper sx={{ width: '100%', mb: 2 }}>
-                                    <TableContainer>
-                                        <Table
-                                            sx={{ minWidth: 750 }}
-                                            aria-labelledby="tableTitle"
-                                            size={'small'}
-                                        >
-                                            <TableHead>
-                                                <TableRow>
-                                                    {headCells.map((headCell) => (
-                                                    <TableCell
-                                                        key={headCell.id}
-                                                        align={headCell.numeric ? 'right' : 'left'}
-                                                        padding={headCell.disablePadding ? 'none' : 'normal'}
-                                                    >
-                                                        {headCell.label}
-                                                    </TableCell>
-                                                    ))}
-                                                </TableRow>
-                                            </TableHead>
-                                            <TableBody>
-                                                {visibleRows.map((row, index) => {
-                                                    const labelId = `enhanced-table-checkbox-${index}`;
-                                
-                                                    return (
-                                                        <TableRow
-                                                            hover
-                                                            tabIndex={-1}
-                                                            key={index}
-                                                            sx={{ cursor: 'pointer' }}
-                                                        >
-                                                            <TableCell
-                                                                component="th"
-                                                                id={labelId}
-                                                                scope="row"
-                                                                padding="none"
-                                                            >
-                                                                {row.login}
-                                                            </TableCell>
-                                                            <TableCell align="right">{row.value}</TableCell>
-                                                            <TableCell align="right">{(new Date(row.date)).toLocaleString()}</TableCell>
-                                                            <TableCell align="right">{row.veryfi}</TableCell>
-                                                        </TableRow>
-                                                    );
-                                                })}
-                                                {emptyRows > 0 && (
-                                                    <TableRow
-                                                        style={{
-                                                        height: 33 * emptyRows,
-                                                        }}
-                                                    >
-                                                        <TableCell colSpan={6} />
-                                                    </TableRow>
-                                                )}
-                                            </TableBody>
-                                        </Table>
-                                    </TableContainer>
-                                    <TablePagination
-                                        rowsPerPageOptions={[5, 10, 25]}
-                                        component="div"
-                                        count={rows.length}
-                                        rowsPerPage={rowsPerPage}
-                                        page={page}
-                                        onPageChange={handleChangePage}
-                                        onRowsPerPageChange={handleChangeRowsPerPage}
-                                        labelRowsPerPage={'Строк'}
-                                        labelDisplayedRows={({ from, to, count, page }: {from: number, to: number, count: number, page: number}) => {
-                                            return`${page+1} из ${Math.floor(count/rowsPerPage-0.0001)+1}`
-                                        }}
-                                    />
-                                </Paper>
+        <Box>            
+            {!ready.gold&&<Skeleton variant="rounded" sx={{width: '80%', maxWidth: '800px', height: '60px'}} />}
+            <Fade in={ready.gold}>
+                <Box sx={{    
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignContent: 'center',
+                    alignItems: 'center'}}
+                >
+                    <Accordion sx={{width: width<950?'90%':'80%', maxWidth: '800px'}}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                        >
+                            <Typography>Деньги в кошельках</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center'
+                            }}
+                        >
+                            <Box>
+                                <Typography>Денежек у нас {total}</Typography>
                             </Box>
-                        </Box>
-                    </AccordionDetails>
-                </Accordion>
-            </Box>
-        </Fade>
+                            <Box>
+                                <TextField label="Фильтр по имени" variant="outlined" onChange={({target}: any)=>{
+                                    setFiltr({...filtr, str: target.value})
+                                }} />
+                            </Box>
+                            <Box>
+                                <Box sx={{ width: '100%' }}>
+                                    <Paper sx={{ width: '100%', mb: 2 }}>
+                                        <TableContainer>
+                                            <Table
+                                                sx={{ width: '100%'}}
+                                                aria-labelledby="tableTitle"
+                                                size={'small'}
+                                            >
+                                                <TableHead>
+                                                    <TableRow>
+                                                        {headCells.map((headCell, index) => (
+                                                        ((index<2)||(width>800))&&<TableCell
+                                                            key={headCell.id}
+                                                            align={headCell.numeric ? 'right' : 'left'}
+                                                            padding={headCell.disablePadding ? 'none' : 'normal'}
+                                                        >
+                                                            {headCell.label}
+                                                        </TableCell>
+                                                        ))}
+                                                    </TableRow>
+                                                </TableHead>
+                                                <TableBody>
+                                                    {visibleRows.map((row, index) => {
+                                                        const labelId = `enhanced-table-checkbox-${index}`;
+                                    
+                                                        return (
+                                                            <TableRow
+                                                                hover
+                                                                tabIndex={-1}
+                                                                key={index}
+                                                                sx={{ cursor: 'pointer' }}
+                                                            >
+                                                                <TableCell
+                                                                    component="th"
+                                                                    id={labelId}
+                                                                    scope="row"
+                                                                    padding="none"
+                                                                >
+                                                                    {row.login}
+                                                                </TableCell>
+                                                                <TableCell align="right" sx={{color: row.value>0?'green':'red'}}>{row.value}</TableCell>
+                                                                {(width>800)&&<TableCell align="right" sx={{fontSize: '1rem'}}>{(new Date(row.date)).toLocaleString()}</TableCell>}
+                                                                {(width>800)&&<TableCell align="right">{row.veryfi}</TableCell>}
+                                                                {width<=800&&<TableCell><IconButton><InfoIcon /></IconButton></TableCell>}
+                                                            </TableRow>
+                                                        );
+                                                    })}
+                                                    {emptyRows > 0 && (
+                                                        <TableRow
+                                                            style={{
+                                                            height: 33 * emptyRows,
+                                                            }}
+                                                        >
+                                                            <TableCell colSpan={6} />
+                                                        </TableRow>
+                                                    )}
+                                                </TableBody>
+                                            </Table>
+                                        </TableContainer>
+                                        <TablePagination
+                                            rowsPerPageOptions={[10, 50, 100, 300]}
+                                            component="div"
+                                            count={rows.length}
+                                            rowsPerPage={rowsPerPage}
+                                            page={page}
+                                            onPageChange={handleChangePage}
+                                            onRowsPerPageChange={handleChangeRowsPerPage}
+                                            labelRowsPerPage={'Строк'}
+                                            labelDisplayedRows={({ from, to, count, page }: {from: number, to: number, count: number, page: number}) => {
+                                                return`${page+1} из ${Math.floor(count/rowsPerPage-0.0001)+1}`
+                                            }}
+                                        />
+                                    </Paper>
+                                </Box>
+                            </Box>
+                        </AccordionDetails>
+                    </Accordion>
+                </Box>
+            </Fade>
+        </Box>
+        
     );
 }
